ed4999a65fea5c23ba6953a1982d53f75104c3d4
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-01-15 18:39:00 +0530

    Updating and adding comments

--- string.go
+++ string.go
@@ -7,12 +7,12 @@ import (
 
 func testValidity(s string) (bool, []string) {
 	splits := strings.Split(s, "-")
-	lastPosition := 0
+	lastPosition := 0 // ensure last item is a string
 	for i, split := range splits {
 		_, err := strconv.Atoi(split)
-		if err != nil && i%2 == 0 {
+		if err != nil && i%2 == 0 { // The input is invalid if mod 0 split is not a number
 			return false, nil
-		} else if err == nil && i%2 == 1 {
+		} else if err == nil && i%2 == 1 { // The input is invalid if mod 1 split is a number
 			return false, nil
 		}
 		lastPosition = i
